f41c98641c1aa429d2a49976befd1ba5c686c083
karubits <karubits@proton.me>
2024-09-18 12:11:59 +0900

    updates and additions

---
+++ pterodactyl/panel/README.md
@@ -0,0 +1,9 @@
+# 
+
+
+#### Creating the first user
+
+To create the first user, use the following command. 
+```bash
+docker exec -it pterodactyl_panel php artisan p:user:make
+```
---
+++ pterodactyl/panel/wipe-out.sh
@@ -0,0 +1,4 @@
+rm -rf /opt/containers/data/pterodactyl/var/*
+rm -rf /opt/containers/data/pterodactyl/db/*
+rm -rf /opt/containers/configs/pterodactyl/le/*
+rm -rf /opt/containers/configs/pterodactyl/nginx/*
