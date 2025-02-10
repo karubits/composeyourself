473a3d5b46bffa289e025fefe6db91f1b66758d1
karubits <karubits@proton.me>
2025-02-10 09:59:50 +0900

    various overdue updates

--- dns-stack/dns-stack-up.sh
+++ dns-stack/dns-stack-up.sh
@@ -3,5 +3,5 @@
 IP_OVERLAY=$(ip -br a | grep "tailscale0 " | awk '{print $3}' | cut -d '/' -f 1)
 IP_LAN=$(ip -br a | grep "eth0 " | awk '{print $3}' | cut -d '/' -f 1)
 export IP_OVERLAY IP_LAN
-/usr/bin/docker compose up
+/usr/bin/docker compose up -d
 
